77cd4a1609bad85b1bb7ff23e97484e990a86732
Rodrigo Neto <134714036+RodrigoNet0@users.noreply.github.com>
2024-01-29 19:40:28 +0000

    git init

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,18 @@
+import './Header.css';
+
+
+function Header () {
+
+return(
+    <div className="header-conntainer">
+    <h2 className="title-header">Conheça a Open Rage Solutions</h2>
+    <div className="text-container">
+    <p className="text-header">Uma empresa de tecnologia inovadora e ambiciosa, 
+        dedicada a fornecer soluções de alta qualidade em software e desenvolvimento de sites. 
+        </p>
+</div>
+        </div>
+);
+}
+
+export default Header
